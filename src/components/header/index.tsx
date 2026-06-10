e63f156526d604f8e51b9e003358910cc9858f82
Gabriell-Santos <ferreirag.santos.dev@gmail.com>
2026-06-10 20:04:21 -0300

    criado o header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,30 @@
+import { FiUser, FiLogOut } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import LogoImg from "../../assets/logo.svg";
 export function Header() {
+  const signuser = false;
+  const loadingAuth = false;
   return (
-    <div>
-      <h1>Bem vindo ao site de carros</h1>
+    <div className="w-full flex items-center justify-center mx-auto h-16 bg-gray-100 drop-shadow mb-4">
+      <header className=" w-full flex max-w-7xl items-center justify-between px-2">
+        <Link to={"/"}>
+          <img src={LogoImg} alt="Logo do Site" />
+        </Link>
+        {!loadingAuth && signuser && (
+          <Link to={"/dashboard"}>
+            <div className="border-2 rounded-full p-1">
+              <FiUser size={22} />
+            </div>
+          </Link>
+        )}
+        {!loadingAuth && !signuser && (
+          <Link to={"/login"}>
+            <div className="border-2 rounded-full p-1">
+              <FiLogOut size={22} />
+            </div>
+          </Link>
+        )}
+      </header>
     </div>
   );
 }
